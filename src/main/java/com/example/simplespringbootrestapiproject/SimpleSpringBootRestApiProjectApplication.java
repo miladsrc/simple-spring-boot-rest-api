2d4b2b9805792d4e9595b865e0ad15bd6a694fca
milad <baranimilad70@yahoo.com>
2024-06-19 11:58:09 +0330

    #1-get mappin rest api controller for HelloWordController.java

---
+++ src/main/java/com/example/simplespringbootrestapiproject/SimpleSpringBootRestApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootrestapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootRestApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootRestApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplespringbootrestapiproject/SimpleSpringBootRestApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootrestapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootRestApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
